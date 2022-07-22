2797a845d3fa26f5888f44139512378366e61ec3
xart22 <fajar.rn22@gmail.com>
2022-07-22 14:22:19 +0700

    update cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
